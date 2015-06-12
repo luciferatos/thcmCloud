243767108a687b870d594670e2da18b534b7d6b1
luciferatos <ubirajara_neto@hotmail.com>
2015-06-12 11:21:14 -0300

    Update application.js
    Tenta seguir mais ou menos esse exemplo

--- public/application.js
+++ public/application.js
@@ -20,3 +20,26 @@ if(window.location.hash === '#_=_') window.location.hash = '#!';
 angular.element(document).ready(function(){
   angular.bootstrap(document, [mainApplicationModuleName]);
 });
+
+// Mudança proposta---------------------------------------------------------
+angular.module('scotchy', [])
+    
+    .config(function($routeProvider, $locationProvider) {
+
+        $routeProvider
+            .when('/', {
+                templateUrl : 'partials/home.html',
+                controller : mainController
+            })
+            .when('/about', {
+                templateUrl : 'partials/about.html',
+                controller : mainController
+            })
+            .when('/contact', {
+                templateUrl : 'partials/contact.html',
+                controller : mainController
+            });
+    
+        // use the HTML5 History API
+        $locationProvider.html5Mode(true);
+    });
